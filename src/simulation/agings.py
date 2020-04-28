bd7b159fd7aa2da6b79a0f63469242922ae0293c
siard <siardkeulen@gmail.com>
2020-04-28 14:09:54 +0200

    Fixed agings iteration function.

--- src/simulation/agings.py
+++ src/simulation/agings.py
@@ -7,14 +7,14 @@ class Agings:
         omegas[alive_components] = 100 * np.random.weibull(5, np.sum(alive_components)) # repr. iterations on 100% usage when cpu will fail
 
         self.lambdas = np.divide(1, omegas, out=np.zeros_like(omegas), where=omegas != 0)
-        self.agings = np.zeros(alive_components.shape, dtype=np.float)  # Will increment each iteration
+        self.cur_agings = np.zeros(alive_components.shape, dtype=np.float)  # Will increment each iteration
 
     def update_agings(self, alive_components, thermals):
         """ Update the aging values of all components with a single iteration.
 
         :return: None
         """
-        self.agings[alive_components] += self.lambdas[alive_components] * (thermals / 100)
+        self.cur_agings[alive_components] += (self.lambdas * thermals / 100)[alive_components]
 
     def iterate(self, alive_components, thermals):
         self.update_agings(alive_components, thermals)

6ff362733998a398d8a00e24e3a7e28b1f06ea2a
siard <siardkeulen@gmail.com>
2020-05-02 20:28:30 +0200

    Added idea comments.

--- tests/simulation/test_elements_agings.py
+++ tests/simulation/test_elements_agings.py
@@ -1,1 +1,11 @@
 import pytest
+
+# Things to test:
+# - aging after one iterations with workload is not 0
+# - aging for all locs with no component is 0
+# - aging for all components > 1 results in system failure
+# - thermals 0 means no aging
+
+
+class TestAgings:
+    pass

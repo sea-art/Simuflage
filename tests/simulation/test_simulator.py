234a64f6c0032a5da430b28683f2b3ae166539a8
siard <siardkeulen@gmail.com>
2020-05-04 13:47:24 +0200

    Finalized basic simulation testing.

--- tests/simulation/test_simulator.py
+++ tests/simulation/test_simulator.py
@@ -1,1 +1,28 @@
 import pytest
+
+from design.application import Application
+from design.component import Component
+from design.designpoint import Designpoint
+from simulation.simulator import Simulator
+
+
+class TestSimulator:
+    @staticmethod
+    def get_simulator_example(cap1=100, cap2=100,
+                               loc1=(0, 0), loc2=(1, 1),
+                               app1=50, app2=50):
+        c1 = Component(cap1, loc1)
+        c2 = Component(cap2, loc2)
+
+        a1 = Application(app1)
+        a2 = Application(app2)
+
+        dp = Designpoint([c1, c2], [a1, a2], [(c1, a1), (c2, a2)])
+        return Simulator(dp)
+
+    def test_timestep(self):
+        sim = self.get_simulator_example()
+
+        sim.step()
+
+        assert sim.timesteps == 1

68d028c0a6e92307968226222342468eec547822
sea-art <siardkeulen@gmail.com>
2020-06-24 16:51:10 +0200

    Added Pareto UCB1.

--- src/DSE/evaluation/MAB.py
+++ src/DSE/evaluation/MAB.py
@@ -9,6 +9,8 @@ import math
 import numpy as np
 
 # from DSE import monte_carlo
+from deap.tools import sortNondominated
+
 from src.DSE import monte_carlo
 from simulation import Simulator
 
@@ -19,9 +21,11 @@ __copyright__ = "Copyright 2020 Siard Keulen"
 
 TOP_CANDIDATES = 5
 
+
 ####################
 # SINGLE OBJECTIVE #
 ####################
+
 def mab_so_greedy(designpoints, nr_samples=10000, idx=0, func=max):
     """ Single objective greedy evaluation of the given list of design points.
 
@@ -259,7 +263,6 @@ def mab_so_sar(designpoints, m, nr_samples=1000, idx=0):
 
 
 def compare_mabs(designpoints, nr_samples=2000, idx=0, func=max):
-
     qt = monte_carlo(designpoints, iterations=nr_samples, parallelized=False).values()
 
     values = list(zip(
@@ -294,7 +297,65 @@ def compare_mabs(designpoints, nr_samples=2000, idx=0, func=max):
 
     print(vals)
 
+
 ###################
 # MULTI OBJECTIVE #
 ###################
 
+NR_OBJECTIVES = 3
+
+
+def add_confidence_interval(individuals, N, A_star_len, subtract=False):
+    n = len(individuals)
+    ci = [math.sqrt(2 * math.log(n * (NR_OBJECTIVES * A_star_len) ** (1 / 4)) / N[i]) for i in range(n)]
+
+    for i in range(n):
+        a, b, c = individuals[i].fitness.values
+        if not subtract:
+            individuals[i].fitness.values = (a + ci[i], b + ci[i], c + ci[i])
+        else:
+            individuals[i].fitness.values = (a - ci[i], b - ci[i], c - ci[i])
+
+
+def pareto_ucb1(individuals, k, nr_samples=500):
+    """ Implementation of the pareto UCB1 pseudocode [Drugan&Nowe(2013)]
+
+    :param individuals: individuals provided by the ga (must have fitness attributes)
+    :param k: The number of individuals to select.
+    :return: [(mean of samples, nr_samples)] - will return the mean of the sampled values
+                                               and the amount of samples taken for this dp.
+
+    :return:
+    """
+    n = len(individuals)
+    simulators = {individuals[i]: Simulator(individuals[i]) for i in range(n)}
+    N = {individuals[i]: 1 for i in range(n)}
+
+    # individual fitness values are empirical means
+    for i, indiv in enumerate(individuals):
+        mttf, pow_usage = simulators[indiv].run_optimized()
+        size = individuals[i].evaluate_size()
+        individuals[i].fitness.values = (mttf, pow_usage, size)
+
+    samples = len(individuals)
+
+    while samples < nr_samples:
+        A_star = sortNondominated(individuals, k, first_front_only=True)[0]
+        add_confidence_interval(individuals, list(N.values()), len(A_star))
+
+        A_p = sortNondominated(individuals, k, first_front_only=True)[0]
+        add_confidence_interval(individuals, list(N.values()), len(A_star), subtract=True)
+
+        a = np.random.choice(A_p)
+
+        N[a] += 1
+        samples += 1
+
+        old_mttf, old_usage, old_size = a.fitness.values
+        mttf, usage = simulators[a].run_optimized()
+
+        a.fitness.values = (old_mttf + (mttf - old_mttf) / N[a],
+                            old_usage + (usage - old_usage) / N[a],
+                            old_size)
+
+    return list(zip([i.fitness.values for i in individuals], list(N.values())))
